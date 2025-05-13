0556d237a119fd3712b2209a8446d43b741e03f3
Suthirtha Dey <155644324+Suthirtha2004@users.noreply.github.com>
2025-05-13 10:14:07 +0000

    conevrsion and operations

---
+++ 01_Basics/1_conversion.js
@@ -0,0 +1,65 @@
+/*let score = "33abc";
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);// NaN = not a number
+
+let value = null ;
+console.log(typeof value);
+console.log(typeof(value));
+
+let res = Number(value);
+console.log(typeof res);
+console.log(res);// NaN = not a number
+
+// For boolean value conversion we get 1 or 0
+//For string like names conversion get NAN
+
+let LoggedIn = 1;
+let boolLog = Boolean(LoggedIn);
+console.log(boolLog)
+
+//For conversion of empty string to boolean we get false
+//For conversion of any name or string to boolean we get true
+
+let some = 35;
+let stringNum = String(some);
+console.log(stringNum);
+console.log(typeof stringNum);
+*/
+//*****OPERATIONS*********
+
+let val = 3;
+let negVal = -val;
+console.log(negVal);
+
+/*console.log(2+2);
+console.log(2*2);
+console.log(2-2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%3);*/
+
+let str1 = "hello";
+let str2 = "Archer";
+let str3 = str1+str2;
+console.log(str3)
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+console.log(+true); // True is a boolean one +true converts to number and then converts to 1
+console.log(+""); //Answer is 0 because numm string converted to 0
+
+let num1,num2,num3;
+num1 = num2 = num3 = 2+2;
+
+let gamecounter =100;
+gamecounter++;
+console.log(gamecounter);
+
