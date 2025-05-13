c265d2b9a9537df11650d5b8f118ae46fcc3de2c
Suthirtha Dey <155644324+Suthirtha2004@users.noreply.github.com>
2025-05-13 10:46:55 +0000

    Comparison operations

---
+++ 01_Basics/1_comparison.js
@@ -0,0 +1,21 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 == 1);
+console.log(2!=1);
+console.log(2 < 1);
+
+
+console.log("2" > 1);
+console.log("02" > 0);
+
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0);//null is converted to zero
+
+console.log(undefined == 0)
+
+//Comparison and equality are different sections
+
+//Strict check === This not only checks value but also their datatypes
+
+//console.log("2" === 0); //Not possible checks datatype
