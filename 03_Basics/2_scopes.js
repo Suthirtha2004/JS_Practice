3c1d11505deb6931c3941ca0b449148aa2ad1795
Suthirtha Dey <155644324+Suthirtha2004@users.noreply.github.com>
2025-05-16 15:54:55 +0000

    scopes part 2

--- 03_Basics/2_scopes.js
+++ 03_Basics/2_scopes.js
@@ -16,4 +16,42 @@ console.log(c); //Values in variable can be displayed outside . VAr taked value 
 // { } - Block scope and outside of that is global Scope.In case of block scope it overrides it
 // The global scope in windows console and in block scope are different totally
 
+//Nested Scope
 
+function one(){
+    const username ="Archer"
+
+    function two(){    //two can access the variables of one because one is global to two
+        const website = "JS"
+        console.log(username);
+    }
+    //console.log(website); //one cannot access the variables of two
+
+    two()
+}
+
+one()
+
+if(true){
+    const username = "Archer"
+    if(username === "Archer"){
+        const web = "JS"
+        console.log(username + web)
+    }
+    //console.log(web) outside of scope
+}
+
+//console.log(username); outside of scope
+
+//*************NOTES*******************//
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+ //addtwo(2) - This is the problem which we cannot do here.This is Hoisting
+const addtwo = function(num){ //Expression - A type of function 
+    return num + 2
+}
+
+console.log(addtwo(5))
