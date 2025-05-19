94e6633cadb4360173ff04e6fd217582e98ca7ed
Suthirtha Dey <155644324+Suthirtha2004@users.noreply.github.com>
2025-05-19 07:34:04 +0000

    Switch statements

---
+++ 04_Control/switch.js
@@ -0,0 +1,42 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3;
+switch (month) {
+    case 1:
+        console.log("Month 1")
+        break;
+    case 2:
+        console.log("Month 2")
+        break;
+    case 3:
+        console.log("Month 3")
+        break;
+
+    default:
+        console.log("Default case");
+        break; //Break is essential because if break is not given then the whole code is written after being checked
+}
+
+const m ="march";
+switch (m) {
+    case "jan":
+        console.log("Month 1")
+        break;
+    case "feb":
+        console.log("Month 2")
+        break;
+    case "march":
+        console.log("Month 3")
+        break;
+
+    default:
+        console.log("Default case");
+        break; //Break is essential because if break is not given then the whole code is written after being checked
+}
