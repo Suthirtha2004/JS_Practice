79ac992e813ad2567adc3e95dcd0d1131b60cc73
Suthirtha Dey <155644324+Suthirtha2004@users.noreply.github.com>
2025-05-19 09:03:23 +0000

    while and do while

---
+++ 05_iteration/2_loops.js
@@ -0,0 +1,24 @@
+// while (condition) {
+    
+// }
+let index = 0
+while(index <= 10){
+    console.log(`Value of i : ${index}`)
+    index = index + 2;
+}
+
+let myArray = ["a","b","c"]
+let arr = 0
+while( arr < myArray.length){
+    console.log(`Values are ${myArray[arr]}`);
+    arr++;
+}
+// do {
+    
+// } while (condition);
+let score =11;
+
+do{
+    console.log(`Score is ${score}`);
+    score++;
+}while(score <= 10) //Condition checked after the execution is done
